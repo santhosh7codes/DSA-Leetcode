e82c1bbc663e1bcd3ec94dd91dedf257b1b89e8a
santhosh7codes <v.gokulsanthosh22@gmail.com>
2026-07-02 23:16:33 +0530

    solved Palindrome Number

---
+++ Arrays/TwoSum/palindrome1.py
@@ -0,0 +1,5 @@
+x = input("enter a number : ")
+if x[::-1] == x :
+    print("Palindrome")
+else : 
+    print("not a palindrome98")
--- Arrays/TwoSum/twosumtry1.py
+++ Arrays/TwoSum/twosumtry1.py
@@ -4,4 +4,5 @@ for i in range(0,len(n)+1):
     for i in range(0,len(n+1)):
         if n[i]+ n[j]==value
          print(n[i]+" "+n[j])
-         break
+         brea
+        dflkmlfkmlkfdmb
