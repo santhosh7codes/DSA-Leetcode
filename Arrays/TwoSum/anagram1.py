e24a8e6e3a4ce434cafa758458f5d7e8cd9c6115
santhosh7codes <v.gokulsanthosh22@gmail.com>
2026-07-03 22:35:08 +0530

    checked anagrams

---
+++ Arrays/TwoSum/anagram1.py
@@ -0,0 +1,23 @@
+def isAnagram(a, b):
+    if len(a) != len(b):
+        return False
+    count = {}
+    for ch in a:
+        if ch in count:
+            count[ch] += 1
+        else:
+            count[ch] = 1
+    for ch in b:
+        if ch not in count:
+            return False
+        count[ch] -= 1
+        if count[ch] < 0:
+            return False
+    return True
+a = input("Enter the first string: ")
+b = input("Enter the second string: ")
+
+if isAnagram(a, b):
+    print("The strings are anagrams.")
+else:
+    print("The strings are not anagrams.")
